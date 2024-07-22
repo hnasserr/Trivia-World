34dbf4c0d56dddc6e309bfcb1ba819b3c5eb97d3
hnasserr <h.nasserr0@gmail.com>
2024-07-22 20:13:07 +0200

    categories page adjustment

--- quiz.js
+++ quiz.js
@@ -143,8 +143,8 @@ function displayFinalScore(quizContainer) {
   const message = document.createElement('p');
   
   message.textContent = correctAnswersCount >= 3 ? 
-  'Well done! You did great. 🔥': 
-  'Better luck next time. Keep learning. ☠️'
+  'Well done! You did great Ya aarsa. 🔥': 
+  'Ayre bi khlousak ya wael ☠️'
   
   message.classList.add('message');
   message.classList.add((correctAnswersCount >= 3) ? 'success' : 'fail')
